40ff9b85915b7446f4c56f5fc56469929db36e60
qq1241692733 <1241692733@qq.com>
2021-05-07 23:52:40 +0800

    刷题

---
+++ 刷题记录/14. iNOC产品部-杨辉三角的变形/src/TestDemo.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * 文档注释 一般用于 方法的注释和类的注释
+ * User: hong yaO
+ * Date: 2021-05-2021/5/6
+ * Time: 19:33
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+}
