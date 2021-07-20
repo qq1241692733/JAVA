6282563129832875611e7189a370d85a4d36f4e3
qq1241692733 <1241692733@qq.com>
2021-07-20 23:02:51 +0800

    动态规划/5.字符串分割/.idea/.gitignore

---
+++ 算法/动态规划/5.字符串分割/src/TestDemo.java
@@ -0,0 +1,24 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+     题目描述
+      给定一个字符串和一个词典dict，确定s是否可以根据词典中的词分成
+      一个或多个单词。
+      比如，给定
+      s = "leetcode"
+      dict = ["leet", "code"]
+      返回true，因为"leetcode"可以被分成"leet code"
+ 
+    https://leetcode-cn.com/problems/word-break/
+
+ * User: hong yaO
+ * Date: 2021-07-2021/7/20
+ * Time: 17:04
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+
+
+}
