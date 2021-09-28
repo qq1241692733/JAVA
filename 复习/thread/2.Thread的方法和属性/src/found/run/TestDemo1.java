b54cf488be6e8fde52d1788b7df402844ab28e29
qq1241692733 <1241692733@qq.com>
2021-09-28 21:10:07 +0800

    run和start区别,启动线程和中断线程

---
+++ 复习/thread/2.Thread的方法和属性/src/found/run/TestDemo1.java
@@ -0,0 +1,25 @@
+package found.run;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: hong yaO
+ * Date: 2021-09-2021/9/28
+ * Time: 16:06
+ */
+public class TestDemo1 {
+    /**
+     * run 和 start 的区别：
+          run 属于普通方法，调用了线程任务的方法。而 start 属于启动线程的方法
+          run 方法可以执行多次，而 start 方法只能执行一次
+     */
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            System.out.println("线程名：" +
+                    Thread.currentThread().getName());
+        });
+        thread.start();
+        thread.run();
+    }
+
+}
