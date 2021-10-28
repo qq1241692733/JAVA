ee0dfae193bb9c0990c5b07688ad1ee503d78491
qq1241692733 <1241692733@qq.com>
2021-10-28 22:54:55 +0800

    HashMap的线程不安全问题

---
+++ 复习/thread/9.非线程安全容器/src/TestDemo1.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * 文档注释 一般用于 方法的注释和类的注释
+ * User: hong yaO
+ * Date: 2021-10-2021/10/28
+ * Time: 16:29
+ */
+public class TestDemo1 {
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+
+        ConcurrentHashMap<String,String> concurrentHashMap = new ConcurrentHashMap<>();
+        Hashtable<String,String> hashtable = new Hashtable<>();
+    }
+}
