5deddf5058d05cd50c0ff187b4962c409d554660
qq1241692733 <1241692733@qq.com>
2021-10-31 23:00:00 +0800

    多线程脑图

--- 复习/thread/1.线程的创建/src/FoundThread6.java
+++ 复习/thread/1.线程的创建/src/FoundThread6.java
@@ -24,10 +24,19 @@ public class FoundThread6 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         MyCallable myCallable = new MyCallable();
         // FuntureTask 容器接受返回值  FuntureTask 是 Runnable 的子类
-        FutureTask<Integer> futureTask = new FutureTask<>(myCallable);
+        FutureTask<Integer> futureTask = new FutureTask<>(new Callable() {
+            @Override
+            public Object call() throws Exception {
+                int num = new Random().nextInt(10);
+                System.out.println("子线程：" + Thread.currentThread().getName() +
+                        ", 随机数：" + num);
+                return num;
+            }
+        });
+
         Thread thread = new Thread(futureTask);
         thread.start();
         int res = futureTask.get();
-        System.out.println(String.format("线程名： %s ,数字： %d", Thread.currentThread().getName(), res));
+        System.out.println(String.format("线程名：%s, 线程返回数字： %d", Thread.currentThread().getName(), res));
     }
 }
