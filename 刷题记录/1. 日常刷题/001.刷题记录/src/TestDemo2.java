375c14a3f77eaae0c829c8c60fac4bcb04b83d7f
qq1241692733 <1241692733@qq.com>
2021-11-19 23:08:54 +0800

    时间O(1)判断两个单词是否会有相同字母

--- 刷题记录/1. 日常刷题/001.刷题记录/src/TestDemo2.java
+++ 刷题记录/1. 日常刷题/001.刷题记录/src/TestDemo2.java
@@ -7,4 +7,38 @@
  * Time: 19:01
  */
 public class TestDemo2 {
+    public static void main(String[] args) {
+
+    }
+    public int maxProduct(String[] words) {
+        int maxPor = 0;
+        int[] nums = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            nums[i] = convert(words[i]);
+        }
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                // 判断两单词是否有重复字母，只需要看两个单词对应的二进制数字相与 是否为0
+                if ((nums[i] & nums[j]) == 0)  {
+                    maxPor = Math.max(maxPor, words[i].length() * words[j].length());
+                }
+            }
+        }
+        return maxPor;
+    }
+
+    /**
+     * 单词转为 长度位26的 int数
+     *  'a':   1 = 1<<0
+     *  'b':  10 = 1<<1
+     *  'c': 100 = 1<<2
+     * 'abc': 111 = 1 | 10 | 100
+     */
+    public int convert(String world) {
+        int res = 0;
+        for (int i = 0; i < world.length(); i++) {
+            res |= 1 << (world.charAt(i) - 'a');
+        }
+        return res;
+    }
 }
